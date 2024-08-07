cce154c47c792b7db79f61677ece754371fc1b8b
Ammu-31-25 <anaghabiju559@gmail.com>
2024-08-07 08:11:47 -0500

    mainpage css stripped; added bootstrap

--- mainpage.php
+++ mainpage.php
@@ -1,79 +1,81 @@
 <!DOCTYPE html>
 <html>
 
-<link rel="stylesheet" type="text/css" href="login1.css">
+<!-- <link rel="stylesheet" type="text/css" href="login1.css"> -->
+
 <head>
-<div class="header">
-  <h1>PHARMACIA</h1>
- <p style="margin-top:-20px;line-height:1;font-size:30px;">Pharmacy Management System</p>
-</div>
-<title>
-Pharmacia 
-</title>
+	<title>
+		Pharmacia
+	</title>
+	<!-- copy this line of code -->
+	<link rel="stylesheet" href="bootstrap/node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-
-	<br><br><br><br>
-	<div class="container">
+	<div class="header">
+		<h1 class="display-1">PHARMACIA</h1>
+		<p class="lead">Pharmacy Management System</p>
+	</div>
+	<div class="container-fluid">
 		<form method="post" action="">
 			<div id="div_login">
-				<h1>Admin Login</h1>
-				<center>
+				<h1 class="display-3">Admin Login</h1>
+
 				<div>
 					<input type="text" class="textbox" id="uname" name="uname" placeholder="Username" />
 				</div>
 				<div>
-					<input type="password" class="textbox" id="pwd" name="pwd" placeholder="Password"/>
+					<input type="password" class="textbox" id="pwd" name="pwd" placeholder="Password" />
 				</div>
 				<div>
-				
-				<input type="submit" value="Submit" name="submit" id="submit" />
-				<input type="submit" value="Click here for Pharmacist Login" name="psubmit" id="submit" />
-				
-	<?php
-				
-		include "config.php";
 
-		if(isset($_POST['submit'])){
+					<input type="submit" value="Submit" name="submit" id="submit" />
+					<br>
+					<input type="submit" value="Click here for Pharmacist Login" name="psubmit" id="submit" />
 
-				$uname = mysqli_real_escape_string($conn,$_POST['uname']);
-				$password = mysqli_real_escape_string($conn,$_POST['pwd']);
+					<?php
 
-			if ($uname != "" && $password != ""){
-		
-					$sql="SELECT * FROM admin WHERE a_username='$uname' AND a_password='$password'";
-					$result = $conn->query($sql);
-					$row = $result->fetch_row();
-					if(!$row) {
-						echo "<p style='color:red;'>Invalid username or password!</p>";
+					include "config.php";
+
+					if (isset($_POST['submit'])) {
+
+						$uname = mysqli_real_escape_string($conn, $_POST['uname']);
+						$password = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+						if ($uname != "" && $password != "") {
+
+							$sql = "SELECT * FROM admin WHERE a_username='$uname' AND a_password='$password'";
+							$result = $conn->query($sql);
+							$row = $result->fetch_row();
+							if (!$row) {
+								echo "<p style='color:red;'>Invalid username or password!</p>";
+							} else {
+								session_start();
+								$_SESSION['user'] = $uname;
+								header('location:adminmainpage.php');
+							}
+						}
 					}
-					else {
-						session_start();
-						$_SESSION['user']=$uname;
-						header('location:adminmainpage.php');
+
+					if (isset($_POST['psubmit'])) {
+						header("location:mainpage1.php");
 					}
-				}
-			}
-				
-		if(isset($_POST['psubmit']))
-		{
-			header("location:mainpage1.php");
-		}
-	?>
-			
+					?>
+
 				</div>
-				</center>
+
 			</div>
 		</form>
 	</div>
 
 	<div class=footer>
-	<br>
-	Powered by VE Technologies. 
-	<br><br>
+		<br>
+		Powered by VE Technologies.
+		<br><br>
 	</div>
-	
+	<!-- copy these two lines of code -->
+	<script src="bootstrap/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="bootstrap/node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
 </body>
 
 </html>
--- mainpage1.php
+++ mainpage1.php
@@ -2,79 +2,80 @@
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="login1.css">
-<div class="header">
-<h1>PHARMACIA</h1>
-<p style="margin-top:-20px;line-height:1;font-size:30px;">Pharmacy Management System</p>
-</div>
-<title>
-Pharmacia 
-</title>
+	<!-- <link rel="stylesheet" type="text/css" href="login1.css"> -->
+
+	<title>
+		Pharmacia
+	</title>
+	<link rel="stylesheet" href="bootstrap/node_modules/bootstrap/dist/css/bootstrap.min.css">
+
 </head>
 
 <body>
-
-	<br><br><br><br>
-	<div class="container">
+	<div class="header">
+		<h1>PHARMACIA</h1>
+		<p class="lead">Pharmacy Management System</p>
+	</div>
+	<div class="container-fluid">
 		<form method="post" action="">
 			<div id="div_login">
 				<h1>Pharmacist Login</h1>
-				<center>
+
 				<div>
 					<input type="text" class="textbox" id="uname" name="uname" placeholder="Username" />
 				</div>
 				<div>
-					<input type="password" class="textbox" id="pwd" name="pwd" placeholder="Password"/>
+					<input type="password" class="textbox" id="pwd" name="pwd" placeholder="Password" />
 				</div>
 				<div>
 					<input type="submit" value="Submit" name="submit" id="submit" />
+					<br>
 					<input type="submit" value="Click here for Admin Login" name="psubmit" id="submit" />
 				</div>
-			 
-				
-	<?php
-				
-		include "config.php";
 
-		if(isset($_POST['submit'])){
 
-			$uname = mysqli_real_escape_string($conn,$_POST['uname']);
-			$password = mysqli_real_escape_string($conn,$_POST['pwd']);
+				<?php
 
-			if ($uname != "" && $password != ""){
+				include "config.php";
 
-				$sql="SELECT e_id FROM emplogin WHERE e_username='$uname' AND e_pass='$password'";
-				$result = $conn->query($sql);
-				$row = $result->fetch_row();
-				if(!$row) {
-					echo "<p style='color:red;'>Invalid username or password!</p>";
+				if (isset($_POST['submit'])) {
+
+					$uname = mysqli_real_escape_string($conn, $_POST['uname']);
+					$password = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+					if ($uname != "" && $password != "") {
+
+						$sql = "SELECT e_id FROM emplogin WHERE e_username='$uname' AND e_pass='$password'";
+						$result = $conn->query($sql);
+						$row = $result->fetch_row();
+						if (!$row) {
+							echo "<p style='color:red;'>Invalid username or password!</p>";
+						} else {
+
+							$emp = $row[0];
+							session_start();
+							$_SESSION['user'] = $emp;
+							header("location:pharmmainpage.php");
+						}
+					}
 				}
-				else {
-				
-					$emp=$row[0];
-					session_start();
-					$_SESSION['user']=$emp;
-					header("location:pharmmainpage.php");
-				}
-			}
-		}
-				
-		if(isset($_POST['psubmit']))
-		{
-			header("location:mainpage.php");
-		}
-	?>
 
-				</center> 
+				if (isset($_POST['psubmit'])) {
+					header("location:mainpage.php");
+				}
+				?>
+
+
 			</div>
 		</form>
 	</div>
 	<div class=footer>
-	<br>
-	Powered by VE Technologies. 
-	<br><br>
+		<br>
+		Powered by VE Technologies.
+		<br><br>
 	</div>
-
+	<script src="bootstrap/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="bootstrap/node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
 </body>
 
 </html>
