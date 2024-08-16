a995a4b747eceab93ac765e2ac3c2074a7dc8d6e
StrUnk1209 <strangerunknown1209@gmail.com>
2024-08-16 12:05:58 +0530

    pos2 fixed (hopefully)

--- inventory-add.php
+++ inventory-add.php
@@ -166,14 +166,14 @@
                     <div class="offcanvas-body">
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="adminmainpage.php">Dashboard</a>
+                                <a class="nav-link" aria-current="page" href="adminmainpage.php">Dashboard</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Inventory
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="inventory-add.php">Add New Medicine</a></li>
+                                    <li><a class="dropdown-item active" href="inventory-add.php">Add New Medicine</a></li>
                                     <li><a class="dropdown-item" href="inventory-view.php">Manage Inventory</a></li>
                                 </ul>
                             </li>
